748bd8f3d78accd77c9336e4760865318efe99ec
Yash Goyal <yashgoyalcs@gmail.com>
2022-01-18 07:38:06 +0530

    auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ def create_app():
     app.secret_key = b'\xce\xdd(B\xdd\xf1\x19\x04\x8c\xf0 BV\x93e\x8c'
 
     from app.views.main import main_blueprint
+    from app.views.auth import auth_blueprint
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint)
 
     return app
---
+++ app/views/auth/views.py
@@ -0,0 +1,80 @@
+import uuid
+import logging
+from flask import render_template, request, session, flash, redirect, url_for
+from helpers.utils import is_registered_customer, is_customer
+from controllers.customers_controller import CustomersController
+from . import auth_blueprint
+
+
+@auth_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    if is_registered_customer():
+        return redirect('main.panel')
+    if request.method == 'POST':
+        try:
+            customer = request.form
+            email = customer.get('email')
+            password = customer.get('password')
+
+            # email and password both are mandatory
+            if not email or not password:
+                raise ValueError
+
+            result = CustomersController().find_customer(email=email.lower(), password=password)
+
+            # if not a valid customer found
+            if not result:
+                raise ValueError
+
+            session['cus_id'] = result[0].get('customer_id')
+            session['Registered'] = 'Y'
+            flash('You are Successfully Logged In', 'success')
+            return redirect(url_for('main.panel'))
+        except Exception as err:
+            flash('Email or Password is incorrect', 'danger')
+            logging.error(f'error from login occurred due to {err}')
+
+    return render_template('login.html')
+
+
+@auth_blueprint.route('/logout')
+def logout():
+    session.pop('cus_id', None)
+    session.pop('Registered', None)
+    return redirect(url_for('main.slink'))
+
+
+@auth_blueprint.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if is_registered_customer():
+        return redirect('main.panel')
+    if request.method == 'POST':
+        try:
+            customer = request.form
+            name = customer.get('name')
+            email = customer.get('email')
+            mobile = customer.get('mobile')
+            password = customer.get('password')
+
+            # mobile no. consists only digits
+            if not all((mobile.isnumeric(), email, password)):
+                raise ValueError
+
+            result = CustomersController().check_customer(email=email.lower())
+            if not result:
+                if is_customer():
+                    CustomersController().update_customer(name=name.lower(), email=email.lower(), mobile=mobile.lower(), password=password, cus_id=session['cus_id'])
+                    session['Registered'] = 'Y'
+                else:
+                    cus_id = uuid.uuid4().hex
+                    CustomersController().create_customer(cus_id=cus_id, name=name.lower(), email=email.lower(), mobile=mobile.lower(), password=password.lower())
+                    session['cus_id'] = cus_id
+                    session['Registered'] = 'Y'
+                flash('You are Successfully Registered', 'success')
+                return redirect(url_for('main.panel'))
+            else:
+                flash('This email id is already exist.', 'danger')
+        except Exception as err:
+            flash('Something Went Wrong. Try Again', 'danger')
+            logging.error(f'error from signup occurred due to {err}')
+    return render_template('signup.html')
